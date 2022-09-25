567ade6a9d4e1fe140def1e5fec7ae3889a1ea7d
scrabwind <scrabwind@gmail.com>
2022-09-26 00:35:27 +0200

    Init commit

---
+++ DpiChange.py
@@ -0,0 +1,10 @@
+import win32api
+import win32con
+
+
+def set_dpi(dpi: int):
+    real_value = 1 if dpi == 125 else 0
+    key = "Control Panel\Desktop\PerMonitorSettings\AUS2722L5LMQS083030_14_07E4_8C^A133EEACD46DEB102F26E14D876CFD94"
+    open_key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, key, 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(open_key, "DpiValue", 0, win32con.REG_DWORD, real_value)
+    win32api.RegCloseKey(open_key)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Basicly stolen code from:
+[Change Windows 10/11 Display Resolution with a Python System Tray Application](https://k0nze.dev/posts/change-resolution-python-system-tray/#:~:text=The%20Python%20code%20for%20changing,a%20file%20called%20resolution_switcher.py%20.)  
+I've added change of DPI since I'm basically blind (*joking*) and need to use 2560x1440 in 125% scale
+  
+Might add builtin option to run on startup

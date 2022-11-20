cdb69c1c7dc01b18fb1f62802b7be95e708804d4
scrabwind <scrabwind@gmail.com>
2022-11-20 23:32:41 +0100

    feat: update interval

--- src/clone_extend_monitors.py
+++ src/clone_extend_monitors.py
@@ -8,7 +8,7 @@ def clone_monitors():
     click(3946, 118)
     click(4357, 300)
     click(4357, 250, interval=2)
-    click(917, 630, interval=0.25)
+    click(917, 630, interval=1)
 
 
 def extend_monitors():
@@ -18,4 +18,4 @@ def extend_monitors():
     click(1400, 300)
     click(1800, 500)
     click(1800, 560, interval=2)
-    click(3546, 500, interval=0.25)
+    click(3546, 500, interval=1)
